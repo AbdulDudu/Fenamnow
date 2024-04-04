f6efdd0006dd809442c401a51ffe28c0ddda13ed
salihcodes <salih.codes@proton.me>
2024-04-04 23:06:22 +0300

    feat: added sitemap and corrected nav routes

--- apps/web/modules/common/shared/navbar/index.tsx
+++ apps/web/modules/common/shared/navbar/index.tsx
@@ -43,23 +43,23 @@ export default function Navbar({ isDashboard }: { isDashboard?: boolean }) {
               variant={searchPath == "rental" ? "secondary" : "ghost"}
               asChild
             >
-              <Link href="/search/rental">Rent</Link>
+              <Link href="/search/rental?page=1">Rent</Link>
             </Button>
             <Button
               variant={searchPath == "sale" ? "secondary" : "ghost"}
               asChild
             >
-              <Link href="/search/sale">Buy</Link>
+              <Link href="/search/sale?page=1">Buy</Link>
             </Button>
             <Button
               variant={searchPath == "lease" ? "secondary" : "ghost"}
               asChild
             >
-              <Link href="/search/lease">Lease</Link>
+              <Link href="/search/lease?page=1">Lease</Link>
             </Button>
 
             <Button
-              variant={searchPath == "lease" ? "secondary" : "ghost"}
+              variant={searchPath == "blog" ? "secondary" : "ghost"}
               asChild
             >
               <Link href="/blog">Blog</Link>
