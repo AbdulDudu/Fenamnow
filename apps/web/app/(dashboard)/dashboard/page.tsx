e3511adef32dbe6223828df4410f640193d92342
salihcodes <salih.codes@proton.me>
2024-04-04 23:11:10 +0300

    fix: removed dashboard route

--- apps/web/app/(dashboard)/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import DashboardScreen from "@web/modules/dashboard/settings/components";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Dashboard | Fenamnow"
-};
-export default function DashboardPage() {
-  return <DashboardScreen />;
-}
